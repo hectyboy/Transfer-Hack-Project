74040c1a9f26be63f457926a0d33f26437d1b03d
Agnes Tang <tangagnes67@gmail.com>
2026-04-13 21:14:49 -0700

    added validation to see if major and interests are selected

---
+++ quiz.js
@@ -0,0 +1,28 @@
+function validateForm() {
+    const major = document.getElementById("major").value;
+    const interests = [];
+
+    if (major == "major-select") {
+        alert("Please select a major.");
+        return false;
+    }
+
+    if (document.getElementById("sports").checked) {
+        interests.push("sports");
+    } 
+    if (document.getElementById("music").checked) {
+        interests.push("music");
+    }
+     if (document.getElementById("art").checked) {
+        interests.push("art");
+    } 
+    if (document.getElementById("technology").checked) {
+        interests.push("technology");
+    } 
+    
+    if (interests.length == 0){
+        alert("Please select at least one interest.");
+        return false;
+    }
+    return true;
+}
